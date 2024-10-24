74d3656400ae1eac4b4a6d74bcae511972274b37
lucasoliveira <blesslucas89@gmail.com>
2024-10-24 16:46:29 -0500

    modelo do Profissional done

--- lacrei_saude_api/core/models.py
+++ lacrei_saude_api/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Profissional(models.Model):
+    nome_completo = models.CharField(max_length=255)
+    nome_social = models.CharField(max_length=255, blank=True, null= True)
+    profissao = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=255)
+    contato = models.CharField(max_length=50)
+    
+    
+    def __str__(self):
+        return self.nome_completo
+    
+    
