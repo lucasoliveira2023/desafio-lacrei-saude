c544ce3c8ebe9124bdc4b036f3bc725c2620c246
lucasoliveira <blesslucas89@gmail.com>
2024-10-24 17:09:41 -0500

    feat:vews consulta pronta

--- lacrei_saude_api/core/views.py
+++ lacrei_saude_api/core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from core.models import Profissional, Consulta
+from core.serializers import ProfissionalSerializer, ConsultaSerializers
 # Create your views here.
+
+class ProfissionalViewSet(viewsets.ModelViewSet):
+    queryset = Profissional.objects.all()
+    serializer_class = ProfissionalSerializer
+    
+class ConsultaViewSet(viewsets.ModelViewSet):
+    queryset = Consulta.objects.all()
+    serializer_class = ConsultaSerializers
